a44fb4879ea78ef106bfc40322e5afee9ef12707
crypt-soil <LOIS.24@ichat.sp.edu.sg>
2025-11-06 13:04:44 +0800

    userDAO edited

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -6,12 +6,6 @@ public class UserDAO {
         boolean isValid = false;
 
         try (Connection conn = DBConnection.getConnection()) {
-<<<<<<< Updated upstream
-            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-=======
         	registerModel rm = new registerModel();
         	String hashedPassword = rm.hashPassword(password);
         	System.out.println(hashedPassword);
@@ -21,7 +15,6 @@ public class UserDAO {
             stmt.setString(1, usernameOrEmail);   // username
             stmt.setString(2, usernameOrEmail);   // email
             stmt.setString(3, hashedPassword);    // password
->>>>>>> Stashed changes
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
