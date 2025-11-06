383a34b017870a5614743b0c6611fd106738f26c
crypt-soil <LOIS.24@ichat.sp.edu.sg>
2025-11-06 13:00:54 +0800

    Resolve conflict in UserDAO.java after stash pop

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -2,14 +2,26 @@ package model;
 import java.sql.*;
 
 public class UserDAO {
-    public boolean checkLogin(String username, String password) {
+    public boolean checkLogin(String usernameOrEmail, String password) {
         boolean isValid = false;
 
         try (Connection conn = DBConnection.getConnection()) {
+<<<<<<< Updated upstream
             String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, username);
             stmt.setString(2, password);
+=======
+        	registerModel rm = new registerModel();
+        	String hashedPassword = rm.hashPassword(password);
+        	System.out.println(hashedPassword);
+            String sql = "SELECT * FROM customers WHERE (username = ? OR email = ?) AND password = ?";
+            System.out.println(sql);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, usernameOrEmail);   // username
+            stmt.setString(2, usernameOrEmail);   // email
+            stmt.setString(3, hashedPassword);    // password
+>>>>>>> Stashed changes
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
